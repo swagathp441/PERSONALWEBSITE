b5cf39950ad0c9efd5bcd214f7717be4fdeaf2ee
swagathp441 <128224836+swagathp441@users.noreply.github.com>
2023-03-22 10:04:23 +0530

    Add files via upload

---
+++ email.js
@@ -0,0 +1,20 @@
+function sendMail(e){
+    event.preventDefault();
+    console.log(document.getElementById('name').value)
+    console.log(document.getElementById('email').value)
+    console.log(document.getElementById('msg').value)
+    console.log(document.getElementById('subject').value)
+    let loading=document.querySelector('.loading');
+  
+  var params={
+        
+    from_name:document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    msg: document.getElementById('msg').value,
+    subject:document.getElementById('subject').value
+    
+    
+}
+emailjs.send("service_7rvegsa","template_scbc429",params).then(function(res){
+alert("sucsess"+res.status)})
+  } 
